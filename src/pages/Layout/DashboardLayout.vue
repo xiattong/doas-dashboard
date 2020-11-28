6dd90dbab0dcb4788c01b85346587a51670f3d3a
xiattong <864380220@qq.com>
2020-11-29 02:17:21 +0800

    菜单优化

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -7,13 +7,7 @@
           <p>浓度曲线</p>
         </template>
       </sidebar-link>
-      <sidebar-link v-if="$rtl.mapType === 'map-line'" to="/maps-line">
-        <i class="tim-icons icon-pin"></i>
-        <template v-if="!isRTL">
-          <p>地图</p>
-        </template>
-      </sidebar-link>
-	  <sidebar-link v-if="$rtl.mapType === 'map-wall'" to="/maps-wall">
+	  <sidebar-link :to="$rtl.mapType">
 	    <i class="tim-icons icon-pin"></i>
 	    <template v-if="!isRTL">
 	      <p>地图</p>
